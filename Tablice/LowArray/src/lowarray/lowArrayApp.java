38a46d3897edae887e0181e4d4ea96bd53a6ab0c
Rafal <rafalchojnacki0502@gmail.com>
2017-03-04 15:29:23 +0100

    Demonstracja dzialania klasy o niskopoziomowym interfejsie

---
+++ Tablice/LowArray/src/lowarray/lowArrayApp.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lowarray;
+
+/**
+ *
+ * @author rafal
+ */
+public class LowArray {
+
+    private long[] a;           //referencja do tablicy
+    
+    
+    public LowArray(int size)           //konstruktor
+    {
+        a = new long[size];             //tworzymy tablicę
+    }
+    
+    public void setElem(int index, int value)   //zapisujemy wartość
+    {
+        a[index] = value;
+    }
+    
+    public long getElem(int index)              //pobieramy warość
+    {
+        return a[index];
+    }
+    
+}
+
+    public class lowArrayApp
+    {
+    public static void main(String[] args) {
+        LowArray arr;           //referencja do klasy LowArray
+        arr = new LowArray(100);    //tworzymy obiekt klasy LowArray
+        int nElems = 0;
+        int j;
+        
+        
+        arr.setElem(0,77);
+        arr.setElem(1,99);
+        arr.setElem(2,44);
+        arr.setElem(3,55);
+        arr.setElem(4,22);
+        arr.setElem(5,88);
+        arr.setElem(6,11);
+        arr.setElem(7,00);
+        arr.setElem(8,66);
+        arr.setElem(9,33);          //teraz w tabliy 10 elementow
+        
+        
+    }
+    
+}
