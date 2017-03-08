d91d9d4b226c0ecfdac4334da19088f9e2ed1183
Rafal <rafalchojnacki0502@gmail.com>
2017-03-08 21:59:08 +0100

    Ulepszony interfejs klasy, użytkownik nie musi zajmować się wartościami indeksów. Metody setElem() i getElem() zastąpione insert(),find() i delete()

---
+++ Tablice/HighArrayApp/src/higharrayapp/HighArrayApp.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package higharrayapp;
+
+/**
+ *
+ * @author rafal
+ */
+
+class HighArray
+{
+    private long [] a;      //referencja do tablicy
+    private int nElems;     //liczba elementów w tablicy
+    
+    public HighArray(int max)
+    {
+        a = new long[max];      //tworzymy tablicę
+        nElems =0;              //na razie brak elementów
+    }
+    
+        
+    public boolean find(long searchKey)     //szukamy określonej wartości
+    {
+    int j;
+    for(j=0;j<nElems;j++)       //dla każdego elementu
+        if(a[j] == searchKey)
+            break;
+    if(j == nElems)
+        return false;
+    else
+        return true;
+    }
+    
+    public void insert(long value)      //wstawianie elementu do tablicy
+    {
+        a[nElems] = value;              //wstawiamy
+        nElems++;                       //zwiększamy liczbę elementów
+    }
+    
+    public boolean delete(long value)
+    {
+        int j;
+        for(j=0;j<nElems; j++)          //szukamy elementu
+            if(value == a[j])
+                break;
+        if (j == nElems)                //nie znaleźliśmy
+            return false;
+        else 
+        {
+            for(int k=j; k<nElems; k++) //przesuwamy pozostałe elementy
+                a[k] = a[k+1];
+            nElems--;
+            return true;
+        }    
+    }   //koniec delete()
+    
+    public void display()           //wypisuje zawartość tablicy
+    {
+     for(int j=0; j<nElems; j++)
+         System.out.print(a[j] + " ");
+     System.out.println("");
+    }
+}
+/********************************************/
+
+public class HighArrayApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int maxSize = 100;      //rozmiar tablicy
+        HighArray arr;          //referencja do tablicy
+        arr = new HighArray(maxSize);   //tworzymy tablicę
+        
+        arr.insert(77);
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(33);
+        
+        
+        arr.display();      //wypisujemy elementy
+        
+        int searchKey = 35;     //szukamy elementu 35
+        if(arr.find(searchKey))
+            System.out.println("Znaleziono " + searchKey);
+        else
+            System.out.println("Nie znaleziono " + searchKey);
+        
+        arr.delete(00);         //usuwamy 3 elementy
+        arr.delete(55);
+        arr.delete(99);
+        
+        
+        arr.display();          //wypisujemy elementy ponownie
+        
+    }
+    
+}
